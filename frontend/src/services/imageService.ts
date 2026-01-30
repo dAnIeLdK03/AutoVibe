09ab8b7b2040319ef36cd8159f449a2d4c5d0306
dAnIeLdK03 <danikrustanov@gmail.com>
2026-01-30 14:11:21 +0200

    Created upload service

---
+++ frontend/src/services/imageService.ts
@@ -0,0 +1,9 @@
+import { api } from './api';
+
+
+export const uploadImage = async(file: File): Promise<string> => {
+    const formData = new FormData();
+    formData.append("file", file);
+    const response = await api.post("/cars/upload-image", formData);
+    return response.data;
+}
